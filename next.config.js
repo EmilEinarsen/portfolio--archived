d2d644372ac2f4d18b4957b6f1e0bf13838dae94
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2021-11-17 09:01:40 +0100

    fix(next.config.js): Removed unnecessary wrapper

--- next.config.js
+++ next.config.js
@@ -13,6 +13,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 */
 
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer({
+module.exports = {
   reactStrictMode: true
-})
+}
