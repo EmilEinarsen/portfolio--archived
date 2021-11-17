ce9433aa82722089d6a4b62e4a8fe15be2f2e38e
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2021-11-17 20:32:11 +0100

    fix(Globals.scss): Added comments and changed className "noscroll" to "no-scroll"

--- hooks/useDisableScroll.tsx
+++ hooks/useDisableScroll.tsx
@@ -2,6 +2,6 @@ import { useEffect } from 'react'
 
 export const useDisableScroll = ({ enabled = true, className }: { enabled?: boolean, className?: string }) => useEffect(() => {
 	const html = document.querySelector('html')
-	if(html && enabled) html.classList.add(className ?? 'noscroll');
-	return () => { html && html.classList.remove(className ?? 'noscroll') }
+	if(html && enabled) html.classList.add(className ?? 'no-scroll');
+	return () => { html && html.classList.remove(className ?? 'no-scroll') }
 }, [className, enabled])
